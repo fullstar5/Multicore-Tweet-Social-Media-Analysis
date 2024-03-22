eb3590966466baf1ab974d84dd813ad6dd1fd226
fullstar5 <1518709701@qq.com>
2024-03-22 16:44:28 +1100

    Update A1_optimization1.py

--- A1_optimization1.py
+++ A1_optimization1.py
@@ -87,33 +87,33 @@ with open('./twitter-50mb.json', 'r', encoding='utf-8') as tweet_file:
 #         # with open('output.txt', 'w', encoding='utf-8') as f:
 #         #     f.write('hour: ' + hour + ' day: ' + day + ' sentiment_score: ' + str(sentiment_score))
 #         term += 1
+
+# gather result from children
+gathered_dict_lists = communicator.gather([happiest_hour_dict, happiest_day_dict, most_active_hour_dict,
+                                           most_active_day_dict], root=0)
 #
-# # gather result from children
-# gathered_dict_lists = communicator.gather([happiest_hour_dict, happiest_day_dict, most_active_hour_dict,
-#                                            most_active_day_dict], root=0)
+# todo: merge each dicts
+if rank == 0:
+    for item in gathered_dict_lists:
+        # print(item[0])
+        ans_happiest_hour_dict = merge_dict(ans_happiest_hour_dict, item[0])
+        ans_happiest_day_dict = merge_dict(ans_happiest_day_dict, item[1])
+        ans_most_active_hour_dict = merge_dict(ans_most_active_hour_dict, item[2])
+        ans_most_active_day_dict = merge_dict(ans_most_active_day_dict, item[3])
 #
-# # todo: merge each dicts
-# if rank == 0:
-#     for item in gathered_dict_lists:
-#         # print(item[0])
-#         ans_happiest_hour_dict = merge_dict(ans_happiest_hour_dict, item[0])
-#         ans_happiest_day_dict = merge_dict(ans_happiest_day_dict, item[1])
-#         ans_most_active_hour_dict = merge_dict(ans_most_active_hour_dict, item[2])
-#         ans_most_active_day_dict = merge_dict(ans_most_active_day_dict, item[3])
-#
-#     # todo: sort dicts by items and print output
-#     sorted_happiest_hour = sorted(ans_happiest_hour_dict.items(), key=lambda x: x[1])
-#     sorted_happiest_day = sorted(ans_happiest_day_dict.items(), key=lambda x: x[1])
-#     sorted_active_hour = sorted(ans_most_active_hour_dict.items(), key=lambda x: x[1])
-#     sorted_activate_day = sorted(ans_most_active_day_dict.items(), key=lambda x: x[1])
-#
-#     print("Happiest Hour: " + str(sorted_happiest_hour[-1]))
-#     print("Happiest Day: " + str(sorted_happiest_day[-1]))
-#     print("Most active Hour: " + str(sorted_active_hour[-1]))
-#     print("Most active Day: " + str(sorted_activate_day[-1]))
+    # todo: sort dicts by items and print output
+    sorted_happiest_hour = sorted(ans_happiest_hour_dict.items(), key=lambda x: x[1])
+    sorted_happiest_day = sorted(ans_happiest_day_dict.items(), key=lambda x: x[1])
+    sorted_active_hour = sorted(ans_most_active_hour_dict.items(), key=lambda x: x[1])
+    sorted_activate_day = sorted(ans_most_active_day_dict.items(), key=lambda x: x[1])
+
+    print("Happiest Hour: " + str(sorted_happiest_hour[-1]))
+    print("Happiest Day: " + str(sorted_happiest_day[-1]))
+    print("Most active Hour: " + str(sorted_active_hour[-1]))
+    print("Most active Day: " + str(sorted_activate_day[-1]))
 #
 #     # todo: print time spend
-print(time.time() - start_time)
+    print(time.time() - start_time)
 
 # check output of file
 # with open('output.txt', 'w', encoding='utf-8') as f:
